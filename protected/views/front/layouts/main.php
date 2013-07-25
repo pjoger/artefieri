7bc3e1f9108f98b497be875ec096739f0d5b3d64
PJ <pj@pj-pc>
2013-07-26 00:31:59 +0400

    Лого и другие картинки переехали в папку img. Мыло на домен com

--- protected/views/front/layouts/main.php
+++ protected/views/front/layouts/main.php
@@ -27,7 +27,7 @@
 	    <div id="header-right">
 	    	<div id="kontakt">
 	        	<span><?php echo Yii::t('general', 'Phone');?>: +7 495 555 44 33<br/>
-	            <?php echo Yii::t('general', 'e-Mail');?>: help@artefieri.ru</span>
+	            <?php echo Yii::t('general', 'e-Mail');?>: mail@artefieri.com</span>
 	        </div> <!-- end kontakt -->
 <?php /* ?>
 <!-- 	        <div id="menusearch"> -->
